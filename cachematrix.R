6c368d17312811a8ea6165e3b6cef19aed779de8
wasntnate <notnate@hotmail.com>
2014-04-19 17:17:12 -0700

    Works

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,67 @@
-## Put comments here that give an overall description of what your
-## functions do
+#####
+#
+#  cacheMatrix.R, function for caching matrix inverse results
+#
+#  (c) Nate Bachmeier 2014
+#
+#####
 
-## Write a short comment describing this function
+#
+# Make sure MASS library is loaded 
+library(MASS);
 
+
+# Function to hold computed values
+#   Parameters: x (matrix)
 makeCacheMatrix <- function(x = matrix()) {
 
+  cached <- NULL;
+  
+  # Get the contained value
+  get <- function(){ x;}
+  
+  # Update the contained value
+  set <- function(y){ 
+    x <<- y;
+    cached <<- NULL;
+  }
+  
+  # Get the inverted matrix
+  inverse <- function(){
+    #
+    # If not in cache, compute
+    if(is.null(cached)){
+      cached <- ginv(x);
+    }
+    
+    #return the result
+    invisible(cached);
+  }
+  
+  #
+  # Return to caller the "interface"
+  invisible(list(set=set, 
+                 get=get, 
+                 inverse=inverse));
 }
 
 
-## Write a short comment describing this function
-
+#
+# A wrapper function around makeCacheMatrix
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  #
+  # Create wrapper
+  mcm <- makeCacheMatrix(x);
+  
+  #
+  # Compute Inverse
+  inverseMatrix <- mcm$inverse();
+  
+  #
+  # Call second time to make sure caching worked
+  inverseMatrix2 <-mcm$inverse();
+  
+  #
+  # Return result
+  invisible(inverseMatrix);
 }
